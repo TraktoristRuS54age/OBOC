01714ec342e622868c665e81de81f1807ab4875c
Danchik <124835518+TraktoristRuS54age@users.noreply.github.com>
2024-11-16 00:40:27 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Page()
     <div className={styles.container}>
       {/* Личная информация */}
       <section className={styles.section}>
-        <h1 className={styles.name}>Шереметьев Павел Андреевич</h1>
+        <h1 className={styles.name}>Черепов Даниил Владимирович</h1>
         <p className={styles.jobTitle}>инженер-программист</p>
-        <p className={styles.contactInfo}>Email: SheremetevPvl@yandex.ru</p>
+        <p className={styles.contactInfo}>Email: daniil200126@mail.ru</p>
       </section>
 
       {/* Навыки */}
@@ -21,7 +21,6 @@ export default function Page()
           <li>HTML, CSS</li>
           <li>Git, GitHub</li>
           <li>MySQL, Ne04j, GraphDB</li>
-          <li>RDF, RDFS description</li>
         </ul>
       </section>
 
@@ -29,12 +28,12 @@ export default function Page()
       <section className={styles.section}>
         <h2>Опыт работы</h2>
         <div className={styles.job}>
-          <h3>Разработчик базы знаний для внутри компании</h3>
+          <h3>Разработчик базы знаний и XWiki для внутренних понятий и документации компании</h3>
           <p className={styles.company}>Moneta.ru</p>
           <p className={styles.dates}>Сентябрь 2024 - Настоящее время</p>
           <ul>
-            <li><span className={styles.firstWord}>Стек:</span> Python, ChatGPT API, GraphDB, XWiki, RDFS description</li>
-            <li><span className={styles.firstWord}>Задачи:</span> Разработка базы знаний на основе артефактов компании для внутреннего пользования и навигации, используя LLM</li>
+            <li><span className={styles.firstWord}>Стек:</span> JavaScript, HTML5, CSS, GraphDB, XWiki</li>
+            <li><span className={styles.firstWord}>Задачи:</span> Разработка базы знаний и заполнение страниц XWiki по поиску полученного запроса от сотрудников компании</li>
           </ul>
         </div>
       </section>
@@ -46,12 +45,6 @@ export default function Page()
           <p>ПГТУ, г Йошкар-Ола</p>
           <p>3 курс бакалавриата. Сентябрь 2022 – Настоящее время</p>
           <p>ФИиВТ, Программная инженерия.</p>
-        </div>
-      </section>
-      <section className={styles.section}>
-        <h2>Языки</h2>
-        <div className={styles.education}>
-          <p>Русский, Английский B1.</p>
         </div>
       </section>
     </div>
