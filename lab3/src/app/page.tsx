508da3b940ad4147123a9b3e331806d2c38e9034
Danchik <124835518+TraktoristRuS54age@users.noreply.github.com>
2024-11-15 23:56:18 +0300

    Update page.tsx

--- lab3/src/app/page.tsx
+++ lab3/src/app/page.tsx
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
-export default function Home() {
+export default function Page() 
+{
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}>
+      {/* Личная информация */}
+      <section className={styles.section}>
+        <h1 className={styles.name}>Шереметьев Павел Андреевич</h1>
+        <p className={styles.jobTitle}>инженер-программист</p>
+        <p className={styles.contactInfo}>Email: SheremetevPvl@yandex.ru</p>
+      </section>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      {/* Навыки */}
+      <section className={styles.section}>
+        <h2>Навыки</h2>
+        <ul className={styles.skillsList}>
+          <li>JavaScript, TypeScript</li>
+          <li>C++</li>
+          <li>React, Next.js</li>
+          <li>HTML, CSS</li>
+          <li>Git, GitHub</li>
+          <li>MySQL, Ne04j, GraphDB</li>
+          <li>RDF, RDFS description</li>
+        </ul>
+      </section>
+
+      {/* Опыт работы */}
+      <section className={styles.section}>
+        <h2>Опыт работы</h2>
+        <div className={styles.job}>
+          <h3>Разработчик базы знаний для внутри компании</h3>
+          <p className={styles.company}>Moneta.ru</p>
+          <p className={styles.dates}>Сентябрь 2024 - Настоящее время</p>
+          <ul>
+            <li><span className={styles.firstWord}>Стек:</span> Python, ChatGPT API, GraphDB, XWiki, RDFS description</li>
+            <li><span className={styles.firstWord}>Задачи:</span> Разработка базы знаний на основе артефактов компании для внутреннего пользования и навигации, используя LLM</li>
+          </ul>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      {/* Образование */}
+      <section className={styles.section}>
+        <h2>Образование</h2>
+        <div className={styles.education}>
+          <p>ПГТУ, г Йошкар-Ола</p>
+          <p>3 курс бакалавриата. Сентябрь 2022 – Настоящее время</p>
+          <p>ФИиВТ, Программная инженерия.</p>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h2>Языки</h2>
+        <div className={styles.education}>
+          <p>Русский, Английский B1.</p>
+        </div>
+      </section>
     </div>
   );
 }
